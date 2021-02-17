c22a4e8327f85a55c87d1e09e911cac27c2547a1
Ophy Boamah <ophybee8@gmail.com>
2021-02-17 18:09:25 +0000

    Guest App

---
+++ GuestsReworked.Api/Models/GuestModel.cs
@@ -0,0 +1,9 @@
+namespace GuestsReworked.Api.Models
+{
+    public class GuestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MessageToHost { get; set; }
+    }
+}
---
+++ GuestsReworked.Api/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using GuestsReworked.Api.Models;
+
+namespace GuestsReworked.Api
+{
+    class Program
+    {
+        private static List<GuestModel> guests = new List<GuestModel>();
+        
+        static void Main(string[] args)
+        {
+            GetGuestInfo();
+            PrintGuestInfo();
+            // Console.WriteLine("Hey thereeeee!!!!!!");
+        }
+
+        static void PrintGuestInfo()
+        {
+            foreach (var guest in guests)
+            {
+                Console.WriteLine($"{guest.FirstName} {guest.LastName} said: {guest.MessageToHost}");
+            }
+        }
+
+        static void GetGuestInfo()
+        {
+            string moreGuestsComing = "";
+            do
+            {
+                GuestModel guest = new GuestModel();
+                Console.Write("What's your first name? \n");
+                guest.FirstName = Console.ReadLine();
+                Console.Write("What's your last name? \n");
+                guest.LastName = Console.ReadLine();
+                Console.Write("What's your message to the host? \n");
+                guest.MessageToHost = Console.ReadLine();
+                Console.Write("Are there more guests coming? yes/no \n");
+                moreGuestsComing = Console.ReadLine();
+                
+                guests.Add(guest);
+                Console.Clear();
+            } while (moreGuestsComing.ToLower() == "yes");
+        }
+    }
+}
+
